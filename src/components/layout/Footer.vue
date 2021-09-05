4d275c77697956c4410528cb9a1ce193b425e667
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-09-05 19:17:44 -0300

    Create timeline with History thumb
    Used vuetify slider y thumb

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,11 +1,27 @@
 <template>
   <v-footer dark padless app>
-    <v-col class="text-center" cols="12">
-      {{ new Date().getFullYear() }} — <strong>Curso Vue</strong>
+    <v-col cols="12" class="py-0">
+      <v-slider
+        v-model="value"
+        class="pt-3"
+        readonly
+        value="100"
+        :thumb-size="45"
+        thumb-label="always"
+        thumb-color="blue"
+        color="orange darken-3"
+        ><template v-slot:thumb-label> History </template></v-slider
+      >
     </v-col>
   </v-footer>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      value: 100,
+    };
+  },
+};
 </script>
