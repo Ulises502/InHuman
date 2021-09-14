0c2b72f87e8ddf08fab141f52795b3e0d44bda55
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-09-13 22:18:52 -0300

    Create themes js

---
+++ src/plugins/dark_theme.js
@@ -0,0 +1,11 @@
+//import colors from 'vuetify/lib/util/colors'
+
+export default {
+    primary: '#1976D2',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107',
+}
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import light from './light_theme.js';
+import dark from './dark_theme.js';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: { dark: true },
+    theme: {
+        themes: { light, dark },
+    },
 });
