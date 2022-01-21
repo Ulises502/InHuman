2e5e6b0db4fbde1fe5b0b8e5a5ddedc73dd59482
Francisco Lanciotti <franmellialcubo@hotmail.com>
2022-01-21 19:51:18 -0300

    Change title & delete useless buttons

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,49 +1,11 @@
 <template>
   <div>
     <v-app-bar color="white accent-4" dense app>
-      <v-toolbar-title>App Vue</v-toolbar-title>
+      <v-toolbar-title>InHuman</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-badge
-        :value="hover"
-        color="orange accent-4"
-        left
-        bottom
-        offset-y="0.5rem"
-        transition="slide-x-transition"
-      >
-        <template v-slot:badge>
-          {{ usuario }}
-        </template>
-        <v-hover v-model="hover">
-          <v-btn icon>
-            <v-icon>mdi-account</v-icon>
-          </v-btn>
-        </v-hover>
-      </v-badge>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="option in options"
-            :key="option"
-            @click="() => {}"
-          >
-            <v-list-item-title> {{ option }} </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      
     </v-app-bar>
   </div>
 </template>
