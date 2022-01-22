a0cde590fe3bc1cc7d69fe191e48cb9679f12cbe
Francisco Lanciotti <franmellialcubo@hotmail.com>
2022-01-22 13:34:55 -0300

    Add getters, mutations, actions examples

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>{{ count }}</div>
+  <div>
+    <p>{{ count }} : {{ doneTodosCount }}</p>
+    <v-btn @click="contar">Contar</v-btn>
+  </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    contar() {
+      this.$store.dispatch({
+        type: "incrementAsync",
+        amount: 10,
+      });
+    },
+  },
   computed: {
     count() {
       return this.$store.state.count;
+    },
+    doneTodosCount() {
+      return this.$store.getters.doneTodosCount;
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,39 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
         count: 0,
+        todos: [
+            { id: 1, text: '...', done: true },
+            { id: 2, text: '...', done: false },
+            { id: 3, text: '...', done: true },
+            { id: 4, text: '...', done: true }
+        ]
     },
-    getters: {},
-    mutations: {},
-    actions: {},
+    getters: {
+        doneTodos: (state) => {
+            return state.todos.filter(todo => todo.done)
+        },
+        doneTodosCount: (state, getters) => {
+            return getters.doneTodos.length
+        }
+    },
+    mutations: {
+        increment(state, payload) {
+            state.count += payload.amount
+        }
+    },
+    actions: {
+        increment(context) {
+            context.commit('increment')
+        },
+        incrementAsync({ commit }, payload) {
+            setTimeout(() => {
+                commit({
+                    type: "increment",
+                    amount: payload.amount,
+                })
+            }, 1000)
+        }
+    },
     modules: {},
 });
 
