03c64f9dcbc8e2d8cf337997717bc72a7730940d
Francisco Lanciotti <franmellialcubo@hotmail.com>
2022-01-25 18:27:37 -0300

    Add disabled condition for buying button

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,8 +10,13 @@
       <p v-show="survivalShow">
         Survival: {{ virtues.survival.amount }}
         <span class="info--text mx-2"> ({{ virtues.survival.bought }}) </span
-        ><v-chip small class="ms-3" @click="buy('survival')"
-          >Cost: {{ virtues.survival.cost }}
+        ><v-chip
+          small
+          class="ms-3"
+          @click="buy('survival')"
+          :disabled="survivalBuyDisabled"
+        >
+          Cost: {{ virtues.survival.cost }}
         </v-chip>
       </p>
     </v-card-text>
@@ -44,7 +49,13 @@ export default {
     },
     // Show the survival button if the player has enough humanity
     survivalShow() {
-      return (this.humanity >= 25 || this.virtues.survival.bought >= 1);
+      return this.humanity >= 25 || this.virtues.survival.bought >= 1;
+    },
+
+    // Disabled section
+    // Disable the button if the player doesn't have enough humanity
+    survivalBuyDisabled() {
+      return this.humanity < this.virtues.survival.cost;
     },
   },
 };
