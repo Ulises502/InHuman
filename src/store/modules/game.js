4e56a4b2d5b8a30ce8bd6287b7666e96ad6edf3f
Francisco Lanciotti <franmellialcubo@hotmail.com>
2022-01-31 20:37:53 -0300

    Change messages

--- src/store/modules/game.js
+++ src/store/modules/game.js
@@ -54,7 +54,7 @@ const game = {
             // if ruins is less than 1, then send message
             new Promise((resolve) => {
                 if (rootState.player.ruins == 0) {
-                    dispatch("sendMessage", "People find some old tools in this ancient ruin.\n");
+                    dispatch("sendMessage", "- People find some old tools in this ancient ruin.\n");
                 }
                 resolve()
             }).then(() => commit("player/increaseRuins", { amount: 1 }, { root: true }))
