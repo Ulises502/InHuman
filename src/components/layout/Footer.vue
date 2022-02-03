abaaa028bce8836f2370dbe967cce5ca5ead569e
Francisco Lanciotti <franmellialcubo@hotmail.com>
2022-02-02 21:07:56 -0300

    Add history progress bar

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,8 +1,23 @@
 <template>
   <v-footer dark padless app>
+    <v-progress-linear
+      color="teal"
+      buffer-value="0"
+      value="20"
+      stream
+    ></v-progress-linear>
     <v-col class="text-center" cols="12">
       2022 — <strong>InHuman</strong>
     </v-col>
+    <v-btn
+      fab
+      color="secondary"
+      top
+      right
+      absolute
+    >
+      <v-icon>mdi-script-text-outline</v-icon>
+    </v-btn>
   </v-footer>
 </template>
 
