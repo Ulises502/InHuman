5ed3189e728dc90a8b76ee5d76457c2c3e547b22
Francisco Lanciotti <franmellialcubo@hotmail.com>
2022-02-10 20:03:03 -0300

    Update UpgradeInfo.vue

--- src/components/UpgradeInfo.vue
+++ src/components/UpgradeInfo.vue
@@ -1,9 +1,12 @@
 <template>
-    <div></div>    
+  <div>
+    <v-card-text>
+        <p>What can you do to make life better?</p>
+        <p>Select a Virtue...</p>
+    </v-card-text>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
