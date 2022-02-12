ba8ca38442c319cb75b7cdd21b6c83a5440da145
Francisco Lanciotti <franmellialcubo@hotmail.com>
2022-02-12 10:54:55 -0300

    Add slider for Might Upgrades

--- src/components/Might.vue
+++ src/components/Might.vue
@@ -4,11 +4,51 @@
       >-- Might --</v-card-subtitle
     >
     <v-card-text>
-        https://vuetifyjs.com/en/components/sliders/#append-and-prepend
+      <div class="text--secondary text-center mb-5">Distribute troops to increase Humanity production from Survival (Defence) or Might (Attack). Effect is based on current Might.</div>
+
+      <v-slider
+        v-model="mightBonus"
+        :color="color"
+        track-color="grey"
+        always-dirty
+        min="0"
+        max="100"
+      >
+        <template v-slot:prepend>
+          <v-icon :color="color" @click="decrement">
+            mdi-shield-outline
+          </v-icon>
+        </template>
+
+        <template v-slot:append>
+          <v-icon :color="color" @click="increment"> mdi-sword-cross </v-icon>
+        </template>
+      </v-slider>
     </v-card-text>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    mightBonus: 0,
+  }),
+  methods: {
+    decrement() {
+      this.mightBonus--;
+    },
+    increment() {
+      this.mightBonus++;
+    },
+  },
+  computed: {
+    color() {
+      if (this.mightBonus < 20) return "indigo";
+      if (this.mightBonus < 40) return "teal";
+      if (this.mightBonus < 60) return "green";
+      if (this.mightBonus < 80) return "orange";
+      return "red";
+    },
+  },
+};
 </script>
